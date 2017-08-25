3dc97a604214508b8cde722e4859ddcff4385674
congyou.wu <echo940811@163.com>
2017-08-25 16:10:15 +0800

    start multi peers

--- main.go
+++ main.go
@@ -4,6 +4,9 @@ import (
 	"encoding/json"
 	"fmt"
 	"net/http"
+	"os"
+	"os/signal"
+	"syscall"
 
 	"github.com/golang/groupcache"
 )
@@ -19,7 +22,6 @@ func main() {
 	}))
 	http.HandleFunc("/default", func(rw http.ResponseWriter, r *http.Request) {
 		k := r.URL.Query().Get("key")
-		fmt.Printf("user get %s from groupcache\n", k)
 		var data []byte
 		gp.Get(nil, k, groupcache.AllocatingByteSliceSink(&data))
 		rw.Write(data)
@@ -32,5 +34,21 @@ func main() {
 		}
 		rw.Write([]byte(statsStr))
 	})
-	http.ListenAndServe(":8081", nil)
+	go http.ListenAndServe(":8081", nil)
+	go http.ListenAndServe(":8082", nil)
+	go http.ListenAndServe(":8083", nil)
+	fmt.Println("start ok")
+	ch := make(chan os.Signal, 1)
+	signal.Notify(ch, syscall.SIGHUP, syscall.SIGQUIT, syscall.SIGTERM, syscall.SIGINT, syscall.SIGSTOP)
+	for {
+		s := <-ch
+		fmt.Println("got a signal: " + s.String())
+		switch s {
+		case syscall.SIGQUIT, syscall.SIGTERM, syscall.SIGSTOP, syscall.SIGINT:
+			fmt.Println("quit")
+			return
+		default:
+			return
+		}
+	}
 }
