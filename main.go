6efa83605a8145818ff263df658de65618d26e8a
congyou.wu <echo940811@163.com>
2017-08-25 15:49:03 +0800

    purge and add stats

--- main.go
+++ main.go
@@ -1,36 +1,36 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"net/http"
-	"net/url"
-	"time"
 
 	"github.com/golang/groupcache"
 )
 
 func main() {
-	fmt.Println("ok")
-	self := (&url.URL{Scheme: "http", Host: ":8081"}).String()
+	self := ":8081"
 	pool := groupcache.NewHTTPPool(self)
-	peers := [...]*url.URL{&url.URL{Scheme: "http", Host: ":8082"}, &url.URL{Scheme: "http", Host: ":8083"}}
-	peerStrs := make([]string, 0, len(peers))
-	for _, peer := range peers {
-		peerStrs = append(peerStrs, peer.String())
-	}
-	pool.Set(peerStrs...)
+	peers := []string{":8082", "8083"}
+	pool.Set(peers...)
 	gp := groupcache.NewGroup("default", 64<<20, groupcache.GetterFunc(func(ctx groupcache.Context, key string, dest groupcache.Sink) error {
-		time.Sleep(time.Second * 3)
 		dest.SetString("result for: " + key)
 		return nil
 	}))
-	fmt.Println(gp)
 	http.HandleFunc("/default", func(rw http.ResponseWriter, r *http.Request) {
 		k := r.URL.Query().Get("key")
 		fmt.Printf("user get %s from groupcache\n", k)
 		var data []byte
 		gp.Get(nil, k, groupcache.AllocatingByteSliceSink(&data))
 		rw.Write(data)
+	})
+	http.HandleFunc("/stats", func(rw http.ResponseWriter, r *http.Request) {
+		statsStr, err := json.Marshal(gp.Stats)
+		if err != nil {
+			rw.Write([]byte("server error"))
+			return
+		}
+		rw.Write([]byte(statsStr))
 	})
 	http.ListenAndServe(":8081", nil)
 }
